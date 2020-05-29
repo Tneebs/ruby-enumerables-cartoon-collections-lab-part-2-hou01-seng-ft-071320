caacbc7f5e2ebc005e3baf1dff32aae7ec4bbd15
Ted Neben <tedneben@gmail.com>
2020-05-29 01:53:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,7 @@ end
   
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |string| string.upcase}
-  planeteer_calls = planeteer_calls.each_index { |x| print x, "!"}
+  planeteer_calls.map { |string| string.upcase, "#{string}!"}
 end
 
 
